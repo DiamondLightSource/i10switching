c785edc85bd7785914c10759738ed0dbe5ae8acf
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-06 14:37:20 +0100

    dipole matrix now correct I think

--- matrices.py
+++ matrices.py
@@ -12,37 +12,36 @@ import numpy as np
 ds = 3
 
 #initial position and velocity (along x axis)
-e0 = np.array([[1],
-	       [0]])
+e0 = np.array([1,0])
 
 
 #allow electron to drift between magnets
 drift = np.array([[1,ds],
-		  [0,1]])
+		  [0,1]]) #this matrix is probably wrong way up to work with dot product - to sort
 
 #k is the strength of the magnet and is equal to the CHANGE in velocity so v goes to v+k
 
 
-def dipole(k):
-	return np.array([[np.cos(k),-np.sin(k)],
-			 [np.sin(k),np.cos(k)]])
+def dipole(k,e):
+	rotate = np.array([[np.cos(k), -np.sin(k)],
+			   [np.sin(k), np.cos(k)]])
+	return np.dot(rotate,e)
 
 def convertfwd(e):
-	return e[0]*np.array([np.cos(e[1]),
-			   np.sin(e[1])])
+	return e[0]*np.array([np.cos(e[1]), np.sin(e[1])]) #have I got columns vs rows right?
 
 def convertbck(e):
-	return np.array([[np.sqrt(e[0]**2+e[1]**2)],
-			 [np.arctan(e[1]/e[0])]])
+	return np.array([np.sqrt(e[0]**2+e[1]**2), np.arctan(e[1]/e[0])])
 
 #apply dipole magnet to electron vector
 
+k = np.pi/6
 fwd = convertfwd(e0)
-dipole = dipole(fwd)
-#bck = convertbck(dipole)
+dipole = dipole(k,e0)
+bck = convertbck(dipole)
 #check = convertbck(fwd)
 
-print fwd
+print e0[0]
 
 
 
