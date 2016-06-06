4d319871bbf86a6c55a9b79a8f15fb970ef40630
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-06 17:08:55 +0100

    method now makes sense, kicker strengths are wrong

--- matrices.py
+++ matrices.py
@@ -2,6 +2,8 @@
 
 import dls_packages
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animate
 
 
 #k = strength of kicker --> amount by which velocity is incremented by kick
@@ -18,45 +20,89 @@ class drifting:
 		self.ds = ds
 		self.e = e
 
-	def increment(ds, e):
-		drift = np.array([[1,ds],
+	def increment(self):
+		drift = np.array([[1,self.ds],
 				  [0,1]])
-		return np.dot(drift,e)
+		return np.dot(drift,self.e)
 
 class kicker:
 	def __init__(self,k,e):
 		self.k = k
 		self.e = e
 
-	def dipole(k, e):
-		kick = np.array([0, k])
-		return e + kick
+	def dipole(self):
+		kick = np.array([0, self.k])
+		return self.e + kick
 
 class ID:
 	def __init__(self,e):
 		self.e = e
-	def where:
-		return e
+	def where(self):
+		return self.e
 
 #initialise time step
 t0 = 0
 
-while True:
+for dt in range(10): #while True:
 
-	dt = t0%100
+	#dt = t0%100
 
 	#strength k of magnet varies by sin function
-	strength = [np.sin(dt*np.pi/100), np.sin(dt*np.pi/100), 1, np.sin(dt*np.pi/100+np.pi), np.sin(dt*np.pi/100+np.pi)]
+	strength = [np.sin(dt*np.pi/10), -1.5*np.sin(dt*np.pi/10), 0.5, -1.5*np.sin(dt*np.pi/10+np.pi/2), np.sin(dt*np.pi/10+np.pi/2)] #currently the kicks aren't right - need to work out what to do
 
 	if dt == 0:
 		e = e0
-	move  = drifting.increment(ds,e)
-	path = [kicker.dipole(strength[0],e),move,move,kicker.dipole(strength[1],e),move,ID.where(e),move,kicker.dipole(strength[2],e),move,ID.where(e),move,kicker.dipole(strength[3],e),move,move,kicker.dipole(strength[4],e)]
-	for i in path:
+
+	'''
+	path = [magnet[0].dipole(), move.increment(), move.increment(), magnet[1].dipole(), move.increment(), insertion.where(), move.increment(), magnet[2].dipole(), move.increment(), insertion.where(), move.increment(), magnet[3].dipole(), move.increment(), move.increment(), magnet[4].dipole()]
+
+	x = [e0[0]]
+	for i in range(len(path)):
 		e = path[i]
-	print e
+		x.append(e[0])
+	print x
+	plt.plot(np.arange(16),x)
+#	plt.show()
+	'''
 
+	#x is velocity
+	x = [e0[1]]
+	e = kicker(strength[0],e).dipole()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = kicker(strength[1],e).dipole()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = ID(e).where()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = kicker(strength[2],e).dipole()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = ID(e).where()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = kicker(strength[3],e).dipole()
+	x.append(e[1])
+	e = drifting(ds,e).increment()
+	x.append(e[1])
+	e = kicker(strength[4],e).dipole()
+	x.append(e[1])
 
+	#############
+
+#	print x
+	plt.plot(np.arange(14),x)
+	plt.show()
+
+	dt += 1
+
+#problem with the kick strengths currently - not getting beam correctly back on track, but at least the beam is following instructions, even if they're wrong! :)
 
 
 
