f40d3f68d233456ed93940f85b030e33ec4792a6
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-08 09:48:01 +0100

    Code tidied up using PEP8 rules

--- matrices.py
+++ matrices.py
@@ -1,127 +1,145 @@
 # matrices.py
-# animated simulation of chicane magnets
+# Animated simulation of chicane magnets
 
-# import libraries
+# Import libraries
 
 import dls_packages
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# define matrices to modify the electron beam vector - drift and kicker magnets
 
-class drifting:
-	def __init__(self,ds,e):
-		self.ds = ds
-		self.e = e
+# Define matrices to modify the electron beam vector:
+# drift and kicker magnets.
 
-	def increment(self):
-		drift = np.array([[1,self.ds],
-				  [0,1]])
-		return np.dot(drift,self.e)
 
-class kicker:
-	def __init__(self,k,e):
-		self.k = k
-		self.e = e
+class Drifting:
 
-	def dipole(self):
-		kick = np.array([0, self.k])
-		return self.e + kick
 
-# define the insertion device - this isn't currently used and needs to be added
+    def __init__(self,STEP,e):
+        self.STEP = STEP
+        self.e = e
 
-class ID:
-	def __init__(self,e):
-		self.e = e
-	def where(self):
-		return self.e
+    def increment(self):
+        drift = np.array([[1,self.STEP],
+                          [0,1]])
+        return np.dot(drift,self.e)
 
-# send electron vector through chicane magnets at time t
+
+class Kicker:
+
+
+    def __init__(self,k,e):
+        self.k = k
+        self.e = e
+
+    def dipole(self):
+        kick = np.array([0, self.k])
+        return self.e + kick
+
+
+# Define the insertion device - needs to be added.
+
+
+class InsertionDevice:
+
+
+    def __init__(self,e):
+        self.e = e
+
+    def where(self):
+        return self.e
+
+
+# Send electron vector through chicane magnets at time t.
 
 def timestep(t):
-	# initialise electron beam
-	eBeam = np.array([0,0])
-	eP = [eBeam[0]]
 
-	# set size of step through chicane
-	ds = 1
+    # Initialise electron beam
+    e_beam = np.array([0,0])
+    e_pos = [e_beam[0]]
 
-	# strength k of magnets vary by sin function
-	strength = [np.sin(t*np.pi/100)+1, -(1.5)*(np.sin(t*np.pi/100)+1), 1, -(1.5)*(np.sin(t*np.pi/100+np.pi)+1), np.sin(t*np.pi/100+np.pi)+1]
+    # Set size of step through chicane
+    STEP = 1
+
+    # Strengths of magnets vary by sin function
+    strength = [
+        np.sin(t*np.pi/100) + 1, -(1.5)*(np.sin(t*np.pi/100) + 1), 
+        1, -(1.5)*(np.sin(t*np.pi/100 + np.pi) + 1), 
+        np.sin(t*np.pi/100 + np.pi) + 1
+        ]
 	
-	# path of electron beam through chicane magnets
+    # Path of electron beam through chicane magnets:
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    # First kicker
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_beam = Kicker(strength[0],e_beam).dipole()
+    e_pos.append(e_beam[0])
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    # Second kicker
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_beam = Kicker(strength[1],e_beam).dipole()
+    e_pos.append(e_beam[0])
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])  # insertion device here - to be added later
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    # Third kicker
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_beam = Kicker(strength[2],e_beam).dipole()
+    e_pos.append(e_beam[0])
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])  	# insertion device here - to be added later
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    # Fourth kicker
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_beam = Kicker(strength[3],e_beam).dipole()
+    e_pos.append(e_beam[0])
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    # Fifth kicker
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_beam = Kicker(strength[4],e_beam).dipole()
+    e_pos.append(e_beam[0])
+    # Drift
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
+    e_beam = Drifting(STEP,e_beam).increment()
+    e_pos.append(e_beam[0])
 
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	# first kicker
-	eBeam = drifting(ds,eBeam).increment()
-	eBeam = kicker(strength[0],eBeam).dipole()
-	eP.append(eBeam[0])
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	# second kicker
-	eBeam = drifting(ds,eBeam).increment()
-	eBeam = kicker(strength[1],eBeam).dipole()
-	eP.append(eBeam[0])
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0]) # insertion device here - to be added later
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	# third kicker
-	eBeam = drifting(ds,eBeam).increment()
-	eBeam = kicker(strength[2],eBeam).dipole()
-	eP.append(eBeam[0])
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0]) # insertion device here - to be added later
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	# fourth kicker
-	eBeam = drifting(ds,eBeam).increment()
-	eBeam = kicker(strength[3],eBeam).dipole()
-	eP.append(eBeam[0])
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	# fifth kicker
-	eBeam = drifting(ds,eBeam).increment()
-	eBeam = kicker(strength[4],eBeam).dipole()
-	eP.append(eBeam[0])
-	# drift
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-	eBeam = drifting(ds,eBeam).increment()
-	eP.append(eBeam[0])
-
-	return eP
+    return e_pos
 
 
-# set up figure, axis and plot element to be animated
+# Set up figure, axis and plot element to be animated.
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 16), ylim=(-2, 5))
 line, = ax.plot([], [], lw=2)
 
-# initialization function: plot the background of each frame
+# Initialization function: plot the background of each frame.
 def init():
     line.set_data([], [])
     return line,
 
-# animation function
+# Animation function
 def animate(t):
-    x = np.arange(0, 17) # currently just plotting position of electron beam against integers - to be modified
+    x = np.arange(0, 17)  # currently just plotting position of electron beam against integers - to be modified
     y = timestep(t)
     line.set_data(x, y)
     return line,
 
-# call the animator.  blit=True means only re-draw the parts that have changed.
+# Call the animator.
 anim = animation.FuncAnimation(fig, animate, init_func=init,
                                frames=200, interval=10, blit=True)
 
