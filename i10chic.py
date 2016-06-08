2dde9e482d1b7bffdd2968d94ed4f70589aba8c7
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-08 11:57:29 +0100

    plotted positions of magnets and IDs

--- i10chic.py
+++ i10chic.py
@@ -16,7 +16,7 @@ import matplotlib.animation as animation
 class Drifting:
 
 
-    def __init__(self,STEP):
+    def __init__(self, STEP):
         self.STEP = STEP
 
     def increment(self, e):
@@ -24,11 +24,13 @@ class Drifting:
                           [0,1]])
         return np.dot(drift,e)
 
+    def locate(self,e):
+        return e
 
 class Kicker:
 
 
-    def __init__(self,k,STEP):
+    def __init__(self, k, STEP):
         self.k = k
         self.STEP = STEP
 
@@ -38,21 +40,25 @@ class Kicker:
                           [0,1]])
         return np.dot(drift,e) + kick
 
-#    def locate(self):
-#        return ???
+    def locate(self,e):
+        return e
 
 
 # Define the insertion device - needs to be added.
 
-
 class InsertionDevice:
 
 
-    def __init__(self,e):
-        self.e = e
+    def __init__(self, STEP):
+        self.STEP = STEP
 
-#    def where(self):
-#        return ???
+    def increment(self, e):
+        drift = np.array([[1,self.STEP],
+                          [0,1]])
+        return np.dot(drift,e)
+
+    def locate(self,e):
+        return e #TO WORK OUT HOW TO GET LOCATIONS... NEEDS THINKING ABOUT
 
 
 # Send electron vector through chicane magnets at time t.
@@ -74,71 +80,36 @@ def timestep(t):
         np.sin(t*np.pi/100 + np.pi) + 1
         ]
 
-
-
-    path = [Drifting(STEP),Kicker(strength[0],STEP),Drifting(STEP),Kicker(strength[1],STEP),Drifting(STEP),Drifting(STEP),Drifting(STEP),Kicker(strength[2],STEP),Drifting(STEP),Drifting(STEP),Drifting(STEP),Kicker(strength[3],STEP),Drifting(STEP),Kicker(strength[4],STEP),Drifting(STEP),Drifting(STEP)]
+    path = [
+        Drifting(STEP),Kicker(strength[0],STEP),
+        Drifting(STEP),Kicker(strength[1],STEP),
+        Drifting(STEP),InsertionDevice(STEP),
+        Drifting(STEP),Kicker(strength[2],STEP),
+        Drifting(STEP),InsertionDevice(STEP),
+        Drifting(STEP),Kicker(strength[3],STEP),
+        Drifting(STEP),Kicker(strength[4],STEP),
+        Drifting(STEP),Drifting(STEP)
+        ]
 
     for p in path:
         e_beam = p.increment(e_beam)
         e_pos.append(e_beam[0])
 
-    '''
-    # Path of electron beam through chicane magnets:
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment() #want to increase s by STEP each time # maybe have an overall class that you can call to increment s, apply drift or kicker, add on position...
-    e_pos.append(e_beam[0])
-    # First kicker
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_beam = Kicker(strength[0],e_beam).dipole()
-    e_pos.append(e_beam[0])
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    # Second kicker
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_beam = Kicker(strength[1],e_beam).dipole()
-    e_pos.append(e_beam[0])
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])  # insertion device here - to be added later
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    # Third kicker
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_beam = Kicker(strength[2],e_beam).dipole()
-    e_pos.append(e_beam[0])
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])  	# insertion device here - to be added later
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    # Fourth kicker
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_beam = Kicker(strength[3],e_beam).dipole()
-    e_pos.append(e_beam[0])
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    # Fifth kicker
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_beam = Kicker(strength[4],e_beam).dipole()
-    e_pos.append(e_beam[0])
-    # Drift
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    e_beam = Drifting(STEP,e_beam).increment()
-    e_pos.append(e_beam[0])
-    '''
     return e_pos
 
 # Set up figure, axis and plot element to be animated.
 fig = plt.figure()
 ax = plt.axes(xlim=(0, 16), ylim=(-2, 5))
 line, = ax.plot([], [], lw=2)
+
+# Plot locations of magnets etc
+plt.axvline(x=2,color='k',linestyle='dashed')
+plt.axvline(x=4,color='k',linestyle='dashed')
+plt.axvline(x=8,color='k',linestyle='dashed')
+plt.axvline(x=12,color='k',linestyle='dashed')
+plt.axvline(x=14,color='k',linestyle='dashed')
+plt.plot((6,6), (0, 0), 'r.')
+plt.plot((10,10), (0, 0), 'r.')
 
 # Initialization function: plot the background of each frame.
 def init():
