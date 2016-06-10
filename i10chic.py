161871befd977b61577173b8199a920636beff65
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-10 11:24:55 +0100

    Kicker is no longer explicitly time dependent, it's just an object. Hooray

--- i10chic.py
+++ i10chic.py
@@ -31,11 +31,15 @@ class Drifting:
 class Kicker:
 
 
-    def __init__(self, k):
-        self.k = k
+    def __init__(self, which):
+        self.which = which
 
-    def increment(self, e):
-        kick = np.array([0, self.k])
+    def identify(self):
+        i = self.which
+        return i
+
+    def increment(self, e, k):
+        kick = np.array([0, k])
         return e + kick
 
     def type(self):
@@ -65,7 +69,7 @@ kicker_pos = [pos[1],pos[2],pos[4],pos[6],pos[7]]
 id_pos = [pos[3],pos[5]]
 p_pos = [[pos[3], pos[3]+30],[pos[5],pos[5]+30]]
 
-# Define magnet strength factors (dependent on relative positions and time)
+# Define magnet strength factors (dependent on relative positions and time).
 len1 = pos[2] - pos[1]
 len2 = pos[4] - pos[2]
 d12 = float(len1)/float(len2)
@@ -83,24 +87,8 @@ def strength(t):
         ])
 
     return kick
-'''
-# Define path through magnet
-def get_elements(t):
 
-    kick = strength(t)
-    path = [
-        Drifting(length[0]),Kicker(kick[0]),
-        Drifting(length[1]),Kicker(kick[1]),
-        Drifting(length[2]),InsertionDevice(),
-        Drifting(length[3]),Kicker(kick[2]),
-        Drifting(length[4]),InsertionDevice(),
-        Drifting(length[5]),Kicker(kick[3]),
-        Drifting(length[6]),Kicker(kick[4]),
-        Drifting(length[7])
-        ]
-
-    return path #THIS SHOULDN'T BE IN A FUNCTION AND KICKER STRENGTHS SHOULDN'T BE TIME DEPENDENT HERE - NEEDS SORTING
-'''
+# Define path through system.
 path = [
     Drifting(length[0]),Kicker(0),
     Drifting(length[1]),Kicker(1),
@@ -112,6 +100,9 @@ path = [
     Drifting(length[7])
     ]
 
+# to get strength associated with kicker
+# for Kicker(i) use strength(t)[i]
+# for p in path ... if p.type() == 'kicker' p.increment(e_beam,strength(t)[p.identify()]) else p.increment(e_beam)
 
 
 # Send electron vector through chicane magnets at time t.
@@ -121,19 +112,23 @@ def timestep(t):
     e_beam = np.array([0,0])
     e_loc = [e_beam[0]]
     e_vector = [0,0] # this currently doesn't do anything - need to work out how to use
+
     # Initialise photon beam
     p_vector = []
 
-    path = get_elements(t)
     # Calculate positions of electron beam and photon beam relative to main axis
-    for p in range(len(path)):
-        e_beam = path[p].increment(e_beam)
-        if path[p].type() == 'drift':
+    kick = strength(t)
+    for p in path:
+        if p.type() == 'kicker':
+            e_beam = p.increment(e_beam,kick[p.identify()])
+        elif p.type() == 'drift':
+            e_beam = p.increment(e_beam)
             e_loc.append(e_beam[0])
             e_vector.append([e_beam[0],e_beam[1]]) #must be a nicer way to append this data...
-        if path[p].type() == 'id':
+        elif p.type() == 'id':
+            e_beam = p.increment(e_beam)
             p_vector.append([e_beam[0],e_beam[1]])
-
+    
     return e_vector, p_vector, e_loc  # returns positions and locations but currently just using positions
 '''
 #columns testing
