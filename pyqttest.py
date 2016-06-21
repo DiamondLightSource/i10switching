556c15804fa6c246105c3c44dc879c6fa44605b2
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-06-21 16:37:19 +0100

    more gui experimentation

--- pyqttest.py
+++ pyqttest.py
@@ -27,60 +27,52 @@ class Control(QtGui.QMainWindow):
         QtGui.QToolTip.setFont(QtGui.QFont('SansSerif', 10))
         
         resetButton = QtGui.QPushButton("Reset",self)
+        resetButton.clicked.connect(self.reset)
+        resetButton.clicked.connect(self.plotting)
 
-        graphButton = QtGui.QPushButton("Plot graph",self)
-        graphButton.clicked.connect(self.plotting)
-        graphButton.move(100, 30)
+        kplusButton = QtGui.QPushButton("K +",self)
+        kplusButton.clicked.connect(self.kplus)
+        kplusButton.clicked.connect(self.plotting)
+        kplusButton.move(100, 0)
 
-        kButton = QtGui.QPushButton("K +",self)
-        kButton.clicked.connect(self.kplus)
-        kButton.move(100, 0)
+        kminusButton = QtGui.QPushButton("K -",self)
+        kminusButton.clicked.connect(self.kminus)
+        kminusButton.clicked.connect(self.plotting)
+        kminusButton.move(100,30)
 
         quitButton = QtGui.QPushButton("Quit",self)
         quitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
         quitButton.move(0, 30)
 
-        '''
-        # Layout stuff
-        hbox1 = QtGui.QHBoxLayout()
-        hbox1.addStretch(1)
-        hbox1.addWidget(resetButton)
-
-        hbox = QtGui.QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(kButton)
-        hbox.addWidget(quitButton)
-        
-        vbox = QtGui.QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox1)
-        vbox.addLayout(hbox)
-        self.setLayout(vbox)
-        '''
         self.setGeometry(300, 300, 250, 150)
         self.setWindowTitle('i10chic GUI')    
         self.resize(250, 150)
-        self.center()
-
         self.show()
 
     def kplus(self):
         
         self.k += 1
-        self.plotting()
-        print self.k
+        return self.k
 
-    def center(self):
+    def kminus(self):
         
-        qr = self.frameGeometry()
-        cp = QtGui.QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+        self.k -= 1
+        return self.k
+
+    def reset(self):
         
+        self.k = 10
+        plt.close()
+        plt.plot(self.k,self.k,'.')
+        plt.show()
+        return self.k
+
     def plotting(self):
 
-        plt.plot(np.arange(self.k),np.arange(self.k)) # so just need plot command here, have a separate set data function which is updated by kplus. Should work...
+        plt.plot(self.k,self.k,'.')
+        plt.draw()
         plt.show()
+
         
 def main():
     
