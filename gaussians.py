f5705f0e1a3c08317192ae2dbae1ef3a7b3a7c1c
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-07-05 09:58:14 +0100

    added square wave trigger_

--- gaussians.py
+++ gaussians.py
@@ -11,33 +11,41 @@ import matplotlib.pyplot as plt
 def gaussian(x, a, x0, sigma):
     return a*np.exp(-(x-x0)**2/(2*sigma**2))
 
+# Generate square wave trigger
+xsq = [0,20,80,100]
+ysq = [0,0,1,0]
+
 # Generating clean data
-x1 = np.linspace(0, 10, 100)
-y1 = gaussian(x1, 1, 5, 2)
+x = np.linspace(0, 100, 1000)
+y1 = gaussian(x[:500], 1, xsq[1]+1, 2)
+y2 = gaussian(x[500:], 0.5, xsq[2]+1, 4)
 
-x2 = np.linspace(11, 80, 700)
-y2 = gaussian(x2, 0.5, 50, 4)
 
-x = np.concatenate([x1,x2])
 y = np.concatenate([y1,y2])
 
 # Adding noise to the data
-yn = y + 0.1 * np.random.normal(size=len(x))
+yn = y + 0.02 * np.random.normal(size=len(x))
 
 
 # Plot out the current state of the data and model
 fig = plt.figure()
-ax1 = fig.add_subplot(211)
-scatterplot = ax1.plot(x,y,'.')[0]
-scatterplot.set_ydata(yn)
+ax1 = fig.add_subplot(311)
+scatterplot = [ax1.plot(x,y)[0], ax1.step(xsq,ysq)[0]]
+scatterplot[0].set_ydata(yn)
+plt.ylim(-0.1,1.2)
 
 
-ax2 = fig.add_subplot(212)
+ax2 = fig.add_subplot(312)
 
-subset = [ax2.plot(x[:100],y[:100],'b.')[0], ax2.plot(x[:200],y[400:600],'r.')[0]]  # need to deal with different width issues when overlaying
-subset[0].set_ydata(yn[:100])
-subset[1].set_ydata(yn[400:600])
+#subset = [ax2.plot(np.linspace(-5,5,100),y[:100],'b')[0], ax2.plot(np.linspace(-10,10,200),y[450:650],'r')[0]]  # need to deal with different width issues when overlaying
+#subset[0].set_ydata(yn[:100])
+#subset[1].set_ydata(yn[450:650])
 
+ax3 = fig.add_subplot(313)
+
+for i in range(len(yn)):
+    if yn[i] > 0.1 and x[i] < x[len(x)/2]:
+        print i
 
 
 plt.show()
