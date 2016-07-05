838881ff8b3eb592a122af4ef96f88fd52aa6d86
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-07-05 11:59:47 +0100

    made it easier to modify values so eventually it can take real data

--- gaussians.py
+++ gaussians.py
@@ -1,31 +1,29 @@
 # plots a fake pair of gaussians (and overlays them hopefully)
 import dls_packages
-#import numpy as np
-#import matplotlib.pyplot as plt
-
 import numpy as np
-#from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 # Let's create a function to model and create data
 def gaussian(x, a, x0, sigma):
     return a*np.exp(-(x-x0)**2/(2*sigma**2))
 
+GRAPHRANGE = 100
+GRAPHSCALE = 10
+CENTRESHIFT = 1
+WINDOW = 10
+
 # Generate square wave trigger
-xsq = [0,20,80,100]
+xsq = [0,20,80,GRAPHRANGE]
 ysq = [0,0,1,0]
 
 # Generating clean data
-x = np.linspace(0, 100, 1000)
-y1 = gaussian(x[:500], 1, xsq[1]+1, 2)
-y2 = gaussian(x[500:], 0.5, xsq[2]+1, 4)
-
-
+x = np.linspace(0, GRAPHRANGE, GRAPHRANGE*GRAPHSCALE)
+y1 = gaussian(x[:GRAPHRANGE*GRAPHSCALE/2], 1, xsq[1]+CENTRESHIFT, 2)
+y2 = gaussian(x[GRAPHRANGE*GRAPHSCALE/2:], 0.5, xsq[2]+CENTRESHIFT, 4)
 y = np.concatenate([y1,y2])
 
 # Adding noise to the data
 yn = y + 0.02 * np.random.normal(size=len(x))
-
 
 # Plot out the current state of the data and model
 fig = plt.figure()
@@ -34,18 +32,15 @@ scatterplot = [ax1.plot(x,y)[0], ax1.step(xsq,ysq)[0]]
 scatterplot[0].set_ydata(yn)
 plt.ylim(-0.1,1.2)
 
-
+# Overlay the two gaussians
 ax2 = fig.add_subplot(312)
-
-#subset = [ax2.plot(np.linspace(-5,5,100),y[:100],'b')[0], ax2.plot(np.linspace(-10,10,200),y[450:650],'r')[0]]  # need to deal with different width issues when overlaying
-#subset[0].set_ydata(yn[:100])
-#subset[1].set_ydata(yn[450:650])
+subset1 = np.array(yn[GRAPHSCALE * (xsq[1] - WINDOW + CENTRESHIFT) : GRAPHSCALE * (xsq[1] + WINDOW + CENTRESHIFT)])
+subset2 = np.array(yn[GRAPHSCALE * (xsq[2] - WINDOW + CENTRESHIFT) : GRAPHSCALE * (xsq[2] + WINDOW + CENTRESHIFT)])
+x = np.linspace(0, len(subset1)/GRAPHSCALE, len(subset1))
+ax2.plot(x,subset1)
+ax2.plot(x,subset2)
 
 ax3 = fig.add_subplot(313)
-
-for i in range(len(yn)):
-    if yn[i] > 0.1 and x[i] < x[len(x)/2]:
-        print i
 
 
 plt.show()
