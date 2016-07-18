60cba9a84b198dcd908c54cf5facbd82b703d1b7
Oswald <qzn68189@ws162.diamond.ac.uk>
2016-07-18 10:36:57 +0100

    Landscaped new beamline code

--- i10beamline_ui.py
+++ i10beamline_ui.py
@@ -2,13 +2,13 @@
 
 # i10knobs simplified to see what it's like without the table...
 
-'''
+"""
 Buttons to move I10 fast chicane magnet offsets and scales.
 
 Provides a gui to control magnet scaling and offsets
 in order to allow independant steering of photon and
 electron beams to maintain a closed bump.
-'''
+"""
 
 from pkg_resources import require
 require('cothread==2.10')
@@ -16,13 +16,12 @@ require('scipy==0.10.1')
 require('matplotlib==1.3.1')
 
 import cothread
-from cothread.catools import caget, caput, camonitor, FORMAT_TIME, FORMAT_CTRL
+from cothread.catools import caget, caput, camonitor, FORMAT_TIME #, FORMAT_CTRL
 import numpy
 import scipy.io
-import os, sys
+import os
 import traceback
 
-from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4 import uic
 
@@ -36,12 +35,12 @@ class OverCurrentException(Exception):
 
 class Knobs(object):
 
-    '''
+    """
     Provides an interface to control the I10 Fast Chicane.
     Values stored in the mat file are obtained through a matlab
     middle layer simulation. The values are calculated to enable
     steering of the photon and electron beams.
-    '''
+    """
 
     # Path for matfile loading
     I10_PATH = '/dls_sw/work/common/matlab/i10'
@@ -66,7 +65,9 @@ class Knobs(object):
         'SR10S-PC-CTRL-05']
 
     def __init__(self):
-        '''Setup physics values from matlab files'''
+
+        """Setup physics values from matlab files."""
+
         S = scipy.io.loadmat(os.path.join(self.I10_PATH, 'knobsi10.mat'))
 
         # knob deltas
@@ -100,18 +101,20 @@ class Knobs(object):
         return caget([name + ':ERRG' for name in self.NAMES])
 
     def jog(self, pvs, ofs):
-        '''
-        Incremeants the list of PVs by the offset.
+
+        """
+        Increments the list of PVs by the offset.
         Errors are created when a user is likley to exceed magnet tolerances.
-        '''
+        """
+
         ofs = ofs * self.jog_scale
 
         old_values = caget(pvs)
-        values = old_values + ofs;
+        values = old_values + ofs #;
 
         print
         for name, old, new in zip(pvs, old_values, values):
-            print '%s:\t%f->%f' % (name, old, new);
+            print '%s:\t%f->%f' % (name, old, new) #;
 
         scales = [abs(scale) for scale in self.get_scale()]
         offsets = self.get_offset()
@@ -126,8 +129,8 @@ class Knobs(object):
             scale = scales[n]
             new_val = ofs[n]
             high = offset + new_val + scale
-            low  = offset + new_val - scale
-            if(high > max or low < min):
+            low = offset + new_val - scale
+            if high > max or low < min:
                 print 'Warning: Setting current value above limits:'
                 print ('%s: High: %f\tLow: %f\tMin: %f\tMax: %f\n'
                         % (pvs[n], high, low, max, min))
@@ -137,11 +140,11 @@ class Knobs(object):
 
 class KnobsUi(QtGui.QMainWindow):
 
-    '''
+    """
     Provides the GUI to the underlying Knobs class.
     Relevant status information is also gathered from PVs
     and shown to the user.
-    '''
+    """
 
     UI_FILENAME = 'i10beamlineui.ui'
     I10_ADC_1_PV = 'BL10I-EA-USER-01:WAI1'
@@ -151,10 +154,12 @@ class KnobsUi(QtGui.QMainWindow):
     HIGHLIGHT_COLOR = QtGui.QColor(235, 235, 235) # Light grey
 
     def __init__(self):
-        '''
+
+        """
         Setup UI.
         Connect components and setup all camonitors and associated callbacks.
-        '''
+        """
+
         self.knobs = Knobs()
         QtGui.QMainWindow.__init__(self)
         filename = os.path.join(os.path.dirname(__file__), self.UI_FILENAME)
@@ -166,11 +171,14 @@ class KnobsUi(QtGui.QMainWindow):
         self.ui.bump_right_plus_button.clicked.connect(self.bump2_plus)
         self.ui.bump_right_minus_button.clicked.connect(self.bump2_minus)
 
-        self.ui.small_correction_radiobutton.clicked.connect(lambda: self.set_jog_scaling(0.1))
-        self.ui.full_correcton_radiobutton.clicked.connect(lambda: self.set_jog_scaling(1.0))
+        self.ui.small_correction_radiobutton.clicked.connect(
+                                        lambda: self.set_jog_scaling(0.1))
+        self.ui.full_correcton_radiobutton.clicked.connect(
+                                        lambda: self.set_jog_scaling(1.0))
 
         self.ui.trig = i10plots.Trigger()
-        self.ui.graph = i10plots.WaveformCanvas(self.I10_ADC_1_PV, self.I10_ADC_2_PV)
+        self.ui.graph = i10plots.WaveformCanvas(
+                                        self.I10_ADC_1_PV, self.I10_ADC_2_PV)
 
         self.ui.graph_layout.addWidget(self.ui.trig)
         self.ui.graph_layout.addWidget(self.ui.graph)
@@ -178,10 +186,12 @@ class KnobsUi(QtGui.QMainWindow):
         self.ui.trig.plot_trigger(self.I10_ADC_1_PV)
 
     def jog_handler(self, pvs, ofs):
-        '''
-        Wraps the Knobs.jog method to provide exception handling
+
+        """
+        Wrap the Knobs.jog method to provide exception handling
         in callbacks.
-        '''
+        """
+
         try:
             self.knobs.jog(pvs, ofs)
         except OverCurrentException, e:
@@ -199,7 +209,9 @@ class KnobsUi(QtGui.QMainWindow):
             msgBox.exec_()
 
     def set_jog_scaling(self, scale):
-        '''Change the scaling applied to magnet corrections'''
+
+        """Change the scaling applied to magnet corrections."""
+
         self.knobs.jog_scale = scale
 
     def bump1_plus(self):
